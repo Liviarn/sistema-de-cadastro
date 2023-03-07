1c2a39e87d838969d9d4cdc14b062a9d15326908
Liviarn <104848940+Liviarn@users.noreply.github.com>
2023-03-07 16:40:43 -0400

    Atualizacao para validar o push

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log ("Rodando sistema de gererenciamento de jogos")
+console.log ("Aplicação está conectado ao banco de dados")
+// mais um console de teste:
+console.log("O paulo fez esse commit sem voce ver na aula")
